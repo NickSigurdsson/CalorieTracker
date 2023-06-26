8d0d62a3c2030caf85aa50c9fbe1f3bc5a1b6a67
Jeremy <realwxp02@gmail.com>
2023-06-26 19:41:23 -0400

    First commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+  <nav className="nav">
+    <Link to="/" classname="site-title">myCalorieTracker</Link>
+    <ul>
+        <Link to="About">About</Link>
+        <Link to="/Login">Login</Link>
+    </ul>
+  </nav>
+  )
+};
+
+export default Navbar;
